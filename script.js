d13985bba62cc5c5946e9c5023287b88363ad37c
Taiguy <hayakt@uw.edu>
2019-09-15 18:58:41 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var search_base_url =
+  "https://crossorigin.me/https://api-v3.royaleapi.com/player/#";
+  var token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzA0NSwiaWRlbiI6IjI5OTc1MzkxODEyMzA4MTcyOCIsIm1kIjp7InVzZXJuYW1lIjoiIiwiZGlzY3JpbWluYXRvciI6IiIsImtleVZlcnNpb24iOjB9LCJ0cyI6MTU2ODU5MzAxMzY5MH0.mJHYx-vgJJeY98qFKNPbjWV-nud-PQBCn6JJQpMcTmk";
+
+function getSearchUrl(tag) {
+    return search_base_url + tag;
+}
+
+function getPlayer(tag) {
+    let myurl = getSearchUrl(tag);
+    console.log("searchURL: " + myurl);
+
+    $.ajax({
+        method: "GET",
+        url: myurl,
+        dataType: "json",
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + token
+        },
+        success: function(data) {
+          console.log("success");
+        }
+    });
+}
