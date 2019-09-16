62be5f40e7cf80bc254d9deb592829fb900da631
Taiguy <hayakt@uw.edu>
2019-09-15 19:01:08 -0700

    2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var search_base_url =
-  "https://crossorigin.me/https://api-v3.royaleapi.com/player/#";
+  "https://api-v3.royaleapi.com/player/#";
   var token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzA0NSwiaWRlbiI6IjI5OTc1MzkxODEyMzA4MTcyOCIsIm1kIjp7InVzZXJuYW1lIjoiIiwiZGlzY3JpbWluYXRvciI6IiIsImtleVZlcnNpb24iOjB9LCJ0cyI6MTU2ODU5MzAxMzY5MH0.mJHYx-vgJJeY98qFKNPbjWV-nud-PQBCn6JJQpMcTmk";
 
